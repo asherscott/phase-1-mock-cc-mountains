0d91973522743ccf280cd90e675a382c3dbc4e73
flatlandnick <nicholas.lunn@flatironschool.com>
2022-03-04 00:39:46 -0600

    pseudo

---
+++ pseudo.js
@@ -0,0 +1,27 @@
+// 1. See a random mountain displayed in the #mountain-details div when the page loads. A user will see the mountain's image, name, location and number of likes.
+
+// must determine # of mountains to select a random mountain id
+// make fetch request to get mountain
+// function to display mountain details on the dom
+// select all necessary dom elements(image, name, location, likes)
+// update all dom element proper attributes with the mountain specific info
+
+// 2. See a list of all mountain names at the bottom of the page in the #mountains-list ul when the page loads. A user should be able to click on a mountain in the list, and display the information for that mountain in the #mountain-details div.
+
+// make a fetch request to get all mountains
+// function to iterate over mountains and add them to the dom
+// for each mountain create an li
+// update that lis innerText to have the mountain name
+// add event listener to the li
+// when the li is clicked that mountain's details should display in #mountain-details div
+// for the first challenge we should already have a function that will display mountains details
+// select mountains list
+// add li to the list
+
+// 3. A user should be able to click on the like button and the number that is displayed on the button should increment by 1. This number of likes does not need to persist upon page refresh.
+
+// select like button
+// add event listener to like button
+// call back function should increment likes number
+// select likes
+// reset likes innerText to be its previous value + 1
